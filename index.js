6600da84def629104cc8771127a6ff34bf0b3657
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-10-10 18:04:51 +0530

    :zap: production ready express app

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("backend")
+// this starts the server and listens on port 3000 for connections
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Hello Twitter!')
+})
+
+app.get('/login',(req,res) => {
+    res.send('<h1>Login at chai aur code</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
